2b520ac159787eb857c08a849331bc4d08871a9b
Lennart Melzer <lennart@melzer.it>
2015-11-20 08:53:31 +0100

    [tidy] js

--- browser.js
+++ browser.js
@@ -9,265 +9,219 @@ jQuery.extend(Osc.prototype,jQuery.eventEmitter);
 var osc = new Osc();
 
 function setup(nEditors) {
-    window.WebSocket = window.WebSocket || window.MozWebSocket;
-    var url = 'ws://' + location.hostname + ':8002';
-    console.log("attempting websocket connection to " + url);
-    ws = new WebSocket(url);
-    var socket = new BCSocket(null, { reconnect: true });
-    sjs = new sharejs.Connection(socket);
-    ws.onopen = function () {
-	console.log("extramuros websocket connection opened");
-    };
-    ws.onerror = function () {
-	console.log("ERROR opening extramuros websocket connection");
-    };
-    ws.onmessage = function (m) {
-	var data = JSON.parse(m.data);
-	if(data.type == 'osc') {
-	    var address = data.address.substring(1);
-	    // Tidal-specific double-mappings for incoming /play messages
-	    if(address == "play") {
-		data.args.name = data.args[1];
-		data.args.begin = data.args[3];
-		data.args.end = data.args[4];
-		data.args.speed = data.args[5];
-		data.args.pan = data.args[6];
-		data.args.gain = data.args[14];
-		// full list of parameters at bottom
-	    }
-	    $(osc).trigger(address);
-	    eval( address + "(data.args)");
-	    // ummm... we should check to make sure the function exists first!...
-	}
-	if(data.type == 'js') {
-	    eval(data.code);
-	}
-	if(data.type == 'feedback') {
-	    var fb = $('#feedback');
-	    var oldText = fb.val();
-	    if(oldText.length > 10000) oldText = ""; // short-term solution...
-	    fb.val(oldText+data.text);
-	    fb.scrollTop(fb[0].scrollHeight);
-	}
-    };
-    for(var x=1;x<=nEditors;x++) openEditor('edit' + x.toString());
-    setupKeyboardHandlers();
-    setupVisuals();
-    populateModeSelectors();
+  window.WebSocket = window.WebSocket || window.MozWebSocket;
+  var url = 'ws://' + location.hostname + ':8002';
+  console.log("attempting websocket connection to " + url);
+  ws = new WebSocket(url);
+  var socket = new BCSocket(null, { reconnect: true });
+  sjs = new sharejs.Connection(socket);
+  ws.onopen = function () {
+    console.log("extramuros websocket connection opened");
+  };
+  ws.onerror = function () {
+    console.log("ERROR opening extramuros websocket connection");
+  };
+  ws.onmessage = function (m) {
+    var data = JSON.parse(m.data);
+    if(data.type == 'osc') {
+      var address = data.address.substring(1);
+      // Tidal-specific double-mappings for incoming /play messages
+      if(address == "play") {
+	data.args.name = data.args[1];
+	data.args.begin = data.args[3];
+	data.args.end = data.args[4];
+	data.args.speed = data.args[5];
+	data.args.pan = data.args[6];
+	data.args.gain = data.args[14];
+	// full list of parameters at bottom
+      }
+      $(osc).trigger(address);
+      eval( address + "(data.args)");
+      // ummm... we should check to make sure the function exists first!...
+    }
+    if(data.type == 'js') {
+      eval(data.code);
+    }
+    if(data.type == 'feedback') {
+      var fb = $('#feedback');
+      var oldText = fb.val();
+      if(oldText.length > 10000) oldText = ""; // short-term solution...
+      fb.val(oldText+data.text);
+      fb.scrollTop(fb[0].scrollHeight);
+    }
+  };
+  for(var x=1;x<=nEditors;x++) openEditor('edit' + x.toString());
+  setupKeyboardHandlers();
+  setupVisuals();
+  populateModeSelectors();
 }
 
 
 function getPassword() {
-    return password;
+  return password;
 }
 
 function queryPassword() {
-    var dialog = document.getElementById('password-dialog');
+  var dialog = document.getElementById('password-dialog');
 
-    dialog.className = "active"; // show
-    dialog.querySelector("form").onsubmit = function(e) {
-        var x = dialog.querySelector("#password").value;
-        if(x == null || x == "") {
-	    alert("You must enter a password to evaluate code.");
-	    return null;
-        }
-        password = x
-        dialog.className = ""; // hide
-        document.getElementById("global").className = "active";
-        var numEditors = document.querySelector("body").dataset.editors
-        setup(numEditors);
-        e.preventDefault();
+  dialog.className = "active"; // show
+  dialog.querySelector("form").onsubmit = function(e) {
+    var x = dialog.querySelector("#password").value;
+    if(x == null || x == "") {
+      alert("You must enter a password to evaluate code.");
+      return null;
     }
+    password = x
+    dialog.className = ""; // hide
+    document.getElementById("global").className = "active";
+    var numEditors = document.querySelector("body").dataset.editors
+    setup(numEditors);
+    e.preventDefault();
+  }
 }
 
 queryPassword();
 
 function populateModeSelectors() {
-    var sidebars = [].slice.call(document.querySelectorAll(".editor .sidebar"));
+  var sidebars = [].slice.call(document.querySelectorAll(".editor .sidebar"));
 
-    var mkOption = function(name, value) {
-        var key = name + "-mode";
-        var input = document.createElement('input');
-        var label = document.createElement('label');
-        var text = document.createTextNode(value);
-        input.type = "radio";
-        input.name = key;
-        input.value = value;
-        input.dataset.editor = name;
+  var mkOption = function(name, value) {
+    var key = name + "-mode";
+    var input = document.createElement('input');
+    var label = document.createElement('label');
+    var text = document.createTextNode(value);
+    input.type = "radio";
+    input.name = key;
+    input.value = value;
+    input.dataset.editor = name;
 
-        label.appendChild(input);
-        label.appendChild(text);
+    label.appendChild(input);
+    label.appendChild(text);
 
-        return label;
-    };
-    var mkLabel =
-    sidebars.forEach(function(s) {
-        var name = s.dataset.editor;
-        assert(name.length, "editor name is empty");
-        modes.forEach(function(m) {
-            var opt = mkOption(name, m);
+    return label;
+  };
 
-            s.appendChild(opt);
-        });
+  sidebars.forEach(function(s) {
+    var name = s.dataset.editor;
+    assert(name.length, "editor name is empty");
+    modes.forEach(function(m) {
+      var opt = mkOption(name, m);
 
-        var radios = [].slice.call(s.querySelectorAll("[type=radio]"));
-        radios.forEach(function(r) {
-            r.addEventListener('change', changeMode);
-        });
+      s.appendChild(opt);
     });
 
-
+    var radios = [].slice.call(s.querySelectorAll("[type=radio]"));
+    radios.forEach(function(r) {
+      r.addEventListener('change', changeMode);
+    });
+  });
 }
 
 function changeMode(event) {
-    var name = this.dataset.editor;
-    var mode = this.value;
-    var cm = editors[name];
+  var name = this.dataset.editor;
+  var mode = this.value;
+  var cm = editors[name];
 
-    assert(cm, cm);
+  assert(cm, cm);
 
-    cm.setOption("mode", mode);
-    assert(cm.getOption("mode") == mode, mode);
+  cm.setOption("mode", mode);
+  assert(cm.getOption("mode") == mode, mode);
 }
 
 function evaluateBuffer(name) {
-    var password = getPassword();
-    if(password) {
-        // editors[name].save();
-	var msg = { request: "eval", bufferName: name, password: password };
-        var content = editors[name].getValue();
-        console.log("[buffer:content]", content);
+  var password = getPassword();
+  if(password) {
+    var msg = { request: "eval", bufferName: name, password: password };
+    var content = editors[name].getValue();
+    console.log("[buffer:content]", content);
 
-
-	ws.send(JSON.stringify(msg));
-    }
+    ws.send(JSON.stringify(msg));
+  }
 }
 
 function evaluateJavaScriptGlobally(code) {
-    var password = getPassword();
-    if(password) {
-	var msg = { request: "evalJS", code: code, password: password };
-	ws.send(JSON.stringify(msg));
-    }
+  var password = getPassword();
+  if(password) {
+    var msg = { request: "evalJS", code: code, password: password };
+    ws.send(JSON.stringify(msg));
+  }
 }
 
 function openEditor(name) {
-    var elem = document.getElementById(name);
-    if( elem != null) {
-        var doc = sjs.get('extramuros', name, function(err, data) {
-            if (err) {
-                console.error("[open:editor]", err);
+  var elem = document.getElementById(name);
+  if( elem != null) {
+    var doc = sjs.get('extramuros', name, function(err, data) {
+      if (err) {
+        console.error("[open:editor]", err);
+      }
+      else {
+        console.log("[open:editor]", data);
+      }
+    });
+
+    doc.subscribe();
+    doc.whenReady(function() {
+      if (!doc.type) { doc.create('text'); }
+
+      if (doc.type && doc.type.name === 'text') {
+	elem.disabled = false;
+
+        editors[name] = CodeMirror.fromTextArea(elem, {
+          mode: modes[0],
+          theme: 'rubyblue',
+          inputStyle: 'contenteditable',
+          viewportMargin: Infinity,
+          extraKeys: {
+            'Shift-Enter': function(cm) {
+              console.log("[kbd:eval]", name);
+              evaluateBuffer(name);
+            },
+            'Ctrl-Enter': function(cm) {
+              var code = cm.getValue();
+              eval(code);
+              console.log("[kbd:js:eval]", code);
+            },
+            'Tab': function(cm) {
+              var indentUnit = cm.getOption("indentUnit");
+              console.log("[tab]", indentUnit);
+              var spaces = Array(indentUnit + 1).join(" ");
+              cm.replaceSelection(spaces);
             }
-            else {
-                console.log("[open:editor]", data);
-            }
+          }
         });
-
-       doc.subscribe();
-       doc.whenReady(function() {
-            if (!doc.type) { doc.create('text'); }
-
-            if (doc.type && doc.type.name === 'text') {
-		elem.disabled = false;
-
-                editors[name] = CodeMirror.fromTextArea(elem, {
-                    mode: modes[0],
-                    theme: 'rubyblue',
-                    inputStyle: 'contenteditable',
-                    viewportMargin: Infinity,
-                    extraKeys: {
-                        'Shift-Enter': function(cm) {
-                            console.log("[kbd:eval]", name);
-                            evaluateBuffer(name);
-                        },
-                        'Ctrl-Enter': function(cm) {
-                            var code = cm.getValue();
-                            eval(code);
-                            console.log("[kbd:js:eval]", code);
-                        },
-                        'Tab': function(cm) {
-                            var indentUnit = cm.getOption("indentUnit");
-                            console.log("[tab]", indentUnit);
-                            var spaces = Array(indentUnit + 1).join(" ");
-                            cm.replaceSelection(spaces);
-                        }
-                    }
-                });
-		doc.attachCodeMirror(editors[name]);
-            }
-       });
-    }
+	doc.attachCodeMirror(editors[name]);
+      }
+    });
+  }
 }
 
 function setupKeyboardHandlers() {
-    $('textarea').keydown(function (event) {
-	if(event.which == 13 && event.shiftKey && event.ctrlKey) {
-	    // ctrl+shift+enter: evaluate buffer as Javascript through server
-	    event.preventDefault();
-	    var code = $(this).val();
-	    evaluateJavaScriptGlobally(code);
-	}
-	// else if(event.which == 13 && event.ctrlKey) {
-	//     // ctrl+Enter: evaluate text as JavaScript in local browser
-	//     event.preventDefault();
-	//     var code = $(this).val();
-	//     eval(code);
-        //     console.log("js eval");
-	// }
-	// else if(event.which == 13 && event.shiftKey) {
-	//     // shift+Enter: evaluate buffer globally through the server
-	//     event.preventDefault();
-	//     evaluateBuffer(event.target.id);
-	// }
-	else if(event.which == 67 && event.ctrlKey && event.shiftKey) {
-	    // ctrl+shift+c: global clear() on visuals
-	    event.preventDefault();
-	    evaluateJavaScriptGlobally("clear();");
-	}
-	else if(event.which == 82 && event.ctrlKey && event.shiftKey) {
-	    // ctrl+shift+r: global retick() on visuals
-	    event.preventDefault();
-	    evaluateJavaScriptGlobally("retick();");
-	}
-	else if(event.which == 67 && event.altKey) {
-	    // alt+c: global clear() on visuals
-	    event.preventDefault();
-	    evaluateJavaScriptGlobally("clear();");
-	}
-	else if(event.which == 82 && event.altKey) {
-	    // alt+r: global retick() on visuals
-	    event.preventDefault();
-	    evaluateJavaScriptGlobally("retick();");
-	}
-    });
+  $('textarea').keydown(function (event) {
+    if(event.which == 13 && event.shiftKey && event.ctrlKey) {
+      // ctrl+shift+enter: evaluate buffer as Javascript through server
+      event.preventDefault();
+      var code = $(this).val();
+      evaluateJavaScriptGlobally(code);
+    }
+    else if(event.which == 67 && event.ctrlKey && event.shiftKey) {
+      // ctrl+shift+c: global clear() on visuals
+      event.preventDefault();
+      evaluateJavaScriptGlobally("clear();");
+    }
+    else if(event.which == 82 && event.ctrlKey && event.shiftKey) {
+      // ctrl+shift+r: global retick() on visuals
+      event.preventDefault();
+      evaluateJavaScriptGlobally("retick();");
+    }
+    else if(event.which == 67 && event.altKey) {
+      // alt+c: global clear() on visuals
+      event.preventDefault();
+      evaluateJavaScriptGlobally("clear();");
+    }
+    else if(event.which == 82 && event.altKey) {
+      // alt+r: global retick() on visuals
+      event.preventDefault();
+      evaluateJavaScriptGlobally("retick();");
+    }
+  });
 }
-
-// path = "/play",
-// params = [
-//1// S "sound" Nothing,
-//2// F "offset" (Just 0),
-//3// F "begin" (Just 0),
-//4// F "end" (Just 1),
-//5// F "speed" (Just 1),
-//6// F "pan" (Just 0.5),
-//7// F "velocity" (Just 0),
-//8// S "vowel" (Just ""),
-//9// F "cutoff" (Just 0),
-//10// F "resonance" (Just 0),
-//11// F "accelerate" (Just 0),
-//12// F "shape" (Just 0),
-//13// I "kriole" (Just 0),
-//14// F "gain" (Just 1),
-//15// I "cut" (Just (0)),
-//16// F "delay" (Just (0)),
-//17// F "delaytime" (Just (-1)),
-//18// F "delayfeedback" (Just (-1)),
-//19// F "crush" (Just 0),
-//20// I "coarse" (Just 0),
-//21// F "hcutoff" (Just 0),
-//22// F "hresonance" (Just 0),
-//23// F "bandf" (Just 0),
-//24// F "bandq" (Just 0),
-//25// S "unit" (Just "rate"),
-//26// I "loop" (Just 1)
-// ]
--- server.js
+++ server.js
@@ -17,37 +17,37 @@ var stderr = process.stderr;
 
 // parse command-line options
 var knownOpts = {
-    "password" : [String, null],
-    "http-port" : [Number, null],
-    "zmq-port" : [Number, null],
-    "ws-port" : [Number, null],
-    "osc-port" : [Number, null],
-    "help": Boolean
+  "password" : [String, null],
+  "http-port" : [Number, null],
+  "zmq-port" : [Number, null],
+  "ws-port" : [Number, null],
+  "osc-port" : [Number, null],
+  "help": Boolean
 };
 
 var shortHands = {
-    "p" : ["--password"],
-    "h" : ["--http-port"],
-    "z" : ["--zmq-port"],
-    "w" : ["--ws-port"],
-    "o" : ["--osc-port"]
+  "p" : ["--password"],
+  "h" : ["--http-port"],
+  "z" : ["--zmq-port"],
+  "w" : ["--ws-port"],
+  "o" : ["--osc-port"]
 };
 
 var parsed = nopt(knownOpts,shortHands,process.argv,2);
 
 if(parsed['help']!=null) {
-    stderr.write("extramuros server.js usage:\n");
-    stderr.write(" --help (-h)               this help message\n");
-    stderr.write(" --password [word] (-p)    password to authenticate messages to server (required)\n");
-    stderr.write(" --http-port (-h) [number] TCP port for plain HTTP requests (default: 8000)\n");
-    stderr.write(" --zmq-port (-z) [number]  TCP port for 0mq connections to client (default: 8001)\n");
-    stderr.write(" --ws-port (-w) [number]   TCP port for WebSocket connections to browsers and clients (default: 8002)\n");
-    stderr.write(" --osc-port (-o) [number]  UDP port on which to receive OSC messages (default: none)\n");
-    process.exit(1);
+  stderr.write("extramuros server.js usage:\n");
+  stderr.write(" --help (-h)               this help message\n");
+  stderr.write(" --password [word] (-p)    password to authenticate messages to server (required)\n");
+  stderr.write(" --http-port (-h) [number] TCP port for plain HTTP requests (default: 8000)\n");
+  stderr.write(" --zmq-port (-z) [number]  TCP port for 0mq connections to client (default: 8001)\n");
+  stderr.write(" --ws-port (-w) [number]   TCP port for WebSocket connections to browsers and clients (default: 8002)\n");
+  stderr.write(" --osc-port (-o) [number]  UDP port on which to receive OSC messages (default: none)\n");
+  process.exit(1);
 }
 
 if(process.argv.length<3) {
-    stderr.write("extramuros: use --help to display available options\n");
+  stderr.write("extramuros: use --help to display available options\n");
 }
 
 var httpPort = parsed['http-port'];
@@ -59,12 +59,11 @@ if(wsPort==null) wsPort = 8002;
 var oscPort = parsed['osc-port'];
 var password = parsed['password'];
 if(password == null) {
-    stderr.write("Error: --password option is not optional!\n");
-    process.exit(1);
+  stderr.write("Error: --password option is not optional!\n");
+  process.exit(1);
 }
 var serverdb = livedb.client(livedb.memory());
 var share = sharejs.server.createClient({backend: serverdb });
-//var shareclient =
 
 var server = express();
 server.use(express.static(__dirname));
@@ -78,8 +77,8 @@ var pub = zmq.socket('pub');
 var zmqAddress = "tcp://*:" + zmqPort.toString();
 pub.bind(zmqAddress, function(err) {
   if(err) {
-      console.log(err);
-      process.exit(1);
+    console.log(err);
+    process.exit(1);
   }
   else console.log("extramuros: listening on " + zmqAddress + " for 0mq subscribers");
 });
@@ -93,38 +92,38 @@ var options = {
 
 
 var ssocket = new BCSocket('http://localhost:8000/channel')
-    var sjs = new sharejs.client.Connection(ssocket);
+var sjs = new sharejs.client.Connection(ssocket);
 
 // sjs.debug = true;
 
 server.use(BC(options, function(client) {
-    var stream = new Duplex({ objectMode: true });
-    stream._write = function(chunk, encoding, cb) {
-        client.send(JSON.stringify(chunk));
-        cb();
-    };
+  var stream = new Duplex({ objectMode: true });
+  stream._write = function(chunk, encoding, cb) {
+    client.send(JSON.stringify(chunk));
+    cb();
+  };
 
-    stream._read = function(){};
+  stream._read = function(){};
 
-    stream.on("error", function(err) {
-        client.close(err);
-    });
+  stream.on("error", function(err) {
+    client.close(err);
+  });
 
-    client.on("message", function(data) {
-        stream.push(data);
-        stream.emit("close");
-    });
+  client.on("message", function(data) {
+    stream.push(data);
+    stream.emit("close");
+  });
 
-    client.on("close", function(reason) {
-        stream.push(null);
-        stream.emit("close");
-    });
+  client.on("close", function(reason) {
+    stream.push(null);
+    stream.emit("close");
+  });
 
-    stream.on("end", function() {
-        client.close();
-    });
+  stream.on("end", function() {
+    client.close();
+  });
 
-    share.listen(stream);
+  share.listen(stream);
 }));
 
 
@@ -137,98 +136,98 @@ wss.broadcast = function(data) {
 
 var udp;
 if(oscPort != null) {
-    udp = new osc.UDPPort( {
-	localAddress: "0.0.0.0",
-	localPort: oscPort
-    });
-    stderr.write("extramuros: listening for OSC on UDP port " + udpPort.toString()+"\n");
+  udp = new osc.UDPPort( {
+    localAddress: "0.0.0.0",
+    localPort: oscPort
+  });
+  stderr.write("extramuros: listening for OSC on UDP port " + udpPort.toString()+"\n");
 }
 
 
 
 wss.on('connection',function(ws) {
-    // route incoming OSC back to browsers
-    var udpListener = function(m) {
-	var n = {
-	    'type': 'osc',
-	    'address': m.address,
-	    'args': m.args
-	};
-	try { ws.send(JSON.stringify(n)); }
-	catch(e) { stderr.write("warning: exception in WebSocket send\n"); }
+  // route incoming OSC back to browsers
+  var udpListener = function(m) {
+    var n = {
+      'type': 'osc',
+      'address': m.address,
+      'args': m.args
     };
-    if(udp!=null) udp.addListener("message",udpListener);
+    try { ws.send(JSON.stringify(n)); }
+    catch(e) { stderr.write("warning: exception in WebSocket send\n"); }
+  };
+  if(udp!=null) udp.addListener("message",udpListener);
 
 
-    ws.on("message",function(m) {
-	var n = JSON.parse(m);
-	if(n.request == "eval") {
-	    if(n.password == password) {
-		evaluateBuffer(n.bufferName);
-	    }
-	}
-	if(n.request == "evalJS") {
-	    if(n.password == password) {
-		evaluateJavaScriptGlobally(n.code);
-	    }
-	}
-	if(n.request == "oscFromClient") {
-	    if(n.password == password) {
-		forwardOscFromClient(n.address,n.args);
-	    }
-	}
-	if(n.request == "feedback") {
-	    if(n.password == password) {
-		forwardFeedbackFromClient(n.text);
-	    }
-	}
-    });
-    ws.on("close",function(code,msg) {
-	console.log("");
-        console.log("closing client");
-        ws.close(code);
-	if(udp!=null)udp.removeListener("message",udpListener);
-    });
+  ws.on("message",function(m) {
+    var n = JSON.parse(m);
+    if(n.request == "eval") {
+      if(n.password == password) {
+	evaluateBuffer(n.bufferName);
+      }
+    }
+    if(n.request == "evalJS") {
+      if(n.password == password) {
+	evaluateJavaScriptGlobally(n.code);
+      }
+    }
+    if(n.request == "oscFromClient") {
+      if(n.password == password) {
+	forwardOscFromClient(n.address,n.args);
+      }
+    }
+    if(n.request == "feedback") {
+      if(n.password == password) {
+	forwardFeedbackFromClient(n.text);
+      }
+    }
+  });
+  ws.on("close",function(code,msg) {
+    console.log("");
+    console.log("closing client");
+    ws.close(code);
+    if(udp!=null)udp.removeListener("message",udpListener);
+  });
 
 });
 
 if(udp!=null)udp.open();
 
 function evaluateBuffer(name) {
-    console.log("[eval:buffer]", name);
-    var doc = sjs.get('extramuros', name, function(err, data) {
-        if (err) {
-            console.error("[doc:refresh] failed", err);
-        }
-        else {
-            console.log("[doc:refresh]", data);
-        }
-    });
+  console.log("[eval:buffer]", name);
+  var doc = sjs.get('extramuros', name, function(err, data) {
+    if (err) {
+      console.error("[doc:refresh] failed", err);
+    }
+    else {
+      console.log("[doc:refresh]", data);
+    }
+  });
 
-    doc.subscribe();
+  doc.subscribe();
 
-   doc.whenReady(function() {
-        console.log("[doc:ready]", doc.snapshot);
-       pub.send(doc.snapshot);
-    });
+  doc.whenReady(function() {
+    console.log("[doc:ready]", doc.snapshot);
+    pub.send(doc.snapshot);
+  });
 }
 
 function evaluateJavaScriptGlobally(code) {
-    var n = { 'type': 'js', 'code': code };
-    try { wss.broadcast(JSON.stringify(n)); }
-    catch(e) { stderr.write("warning: exception in WebSocket broadcast\n"); }
+  var n = { 'type': 'js', 'code': code };
+  try { wss.broadcast(JSON.stringify(n)); }
+  catch(e) { stderr.write("warning: exception in WebSocket broadcast\n"); }
 }
 
 function forwardOscFromClient(address,args) {
-    var n = { 'type': 'osc', 'address': address, 'args': args };
-    try { wss.broadcast(JSON.stringify(n)); }
-    catch(e) { stderr.write("warning: exception in WebSocket broadcast\n"); }
+  var n = { 'type': 'osc', 'address': address, 'args': args };
+  try { wss.broadcast(JSON.stringify(n)); }
+  catch(e) { stderr.write("warning: exception in WebSocket broadcast\n"); }
 }
 
 function forwardFeedbackFromClient(text) {
-    var n = { 'type': 'feedback', 'text': text };
-    try { wss.broadcast(JSON.stringify(n)); }
-    catch(e) { stderr.write("warning: exception in WebSocket broadcast\n"); }
+  var n = { 'type': 'feedback', 'text': text };
+  try { wss.broadcast(JSON.stringify(n)); }
+  catch(e) { stderr.write("warning: exception in WebSocket broadcast\n"); }
 }
 
 
